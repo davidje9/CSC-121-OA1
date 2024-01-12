97280f09ff37d8c5a73c4b7d5487d4844e3ab023
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-11 19:37:08 -0500

    Add files via upload

---
+++ 2-5_famous_quote.py
@@ -0,0 +1,2 @@
+message = "Kazuo Ishiguro wrote, \"But God will know the slow tread of an old couple’s love for each other, and understand how black shadows make part of its whole.\""
+print(message)
