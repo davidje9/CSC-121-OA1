97fdab194ed7fbf7b1c6c1e82e0dc1faa62bc6e1
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-11 19:58:21 -0500

    Add files via upload

---
+++ 2-8_number_eight.py
@@ -0,0 +1,15 @@
+#addition
+print(6 + 2)
+#subtraction
+print(2_900_900 - 2_900_892)
+#multiplication
+print(4 * 2)
+#division
+print(64/8)
+
+#assign the result of operations to variables then display
+#a = 6 + 2
+#s = 2_900_900 - 2_900_892
+#m = 4 * 2
+#d = 64 / 8
+#print(f'Addition: {a}\nSubtraction: {s}\nMultiplication: {m}\nDivision: {d}')
