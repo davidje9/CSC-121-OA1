796dbf8606453e1c97bcb40e3370bf553fae2965
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-11 19:28:00 -0500

    Add files via upload

---
+++ 2-2_simple_messages.py
@@ -0,0 +1,5 @@
+message = "I am a simple message for module 2 assignment 2. Check me out!"
+print(message)
+
+message = "The variable changed. Now when I print, this new message will appear."
+print(message)
