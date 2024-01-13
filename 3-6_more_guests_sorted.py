17fdc6963a5105fbeba27fb0619ef9edfeaa7b25
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-13 08:42:49 -0500

    Add files via upload
    Textbook ex. 3-6; Moodle calls it 3-8.

---
+++ 3-6_more_guests_sorted.py
@@ -0,0 +1,25 @@
+guest_list = ['Kurt Vonnegut','Walter Benjamin','Joan Didion','Oscar Wilde']
+
+invite_body = "\nYou are cordially invited to dinner in Asheville, NC\non 30 August 2024\nto celebrate David's birthday.\nCash bar.\nPresents expected."
+
+print("Great news! We found a bigger table for David's birthday, which means more scintillating conversation and, of course, more presents.\n")
+
+guest_list.insert(0, "Keanu Reeves")
+guest_list.insert(2,"Patti Smith")
+guest_list.append("Nick Cave")
+
+invite1 = f"Dear {guest_list[0]}{invite_body}"
+invite2 = f"Dear {guest_list[1]}{invite_body}"
+invite3 = f"Dear {guest_list[2]}{invite_body}"
+invite4 = f"Dear {guest_list[3]}{invite_body}"
+invite5 = f"Dear {guest_list[4]}{invite_body}"
+invite6 = f"Dear {guest_list[5]}{invite_body}"
+invite7 = f"Dear {guest_list[6]}{invite_body}"
+
+print(f"{invite1}\n")
+print(f"{invite2}\n")
+print(f"{invite3}\n")
+print(f"{invite4}\n")
+print(f"{invite5}\n")
+print(f"{invite6}\n")
+print(f"{invite7}\n")
