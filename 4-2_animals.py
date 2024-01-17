b50846b5cadc88b42ef61368f2664aa29d4482b5
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-17 09:23:29 -0500

    Delete 4-2_animals.py

--- 4-2_animals.py
+++
@@ -1,4 +0,0 @@
-animals = ['opossums','raccoons','rabbits','deer']
-for animal in animals:
-	print(f"{animal.title()} are frequent visitors in my backyard.")
-print("All of these animals are welcomed guests!")
