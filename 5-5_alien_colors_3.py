04aa4c043bf55b097b0190cd5af605093478f4fa
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-18 08:32:28 -0500

    Add files via upload

---
+++ 5-5_alien_colors_3.py
@@ -0,0 +1,29 @@
+#exercise showing result of shooting a green alien
+alien_color = "green"
+
+if alien_color == "green":
+    print("Nice shot! You earned 5 points.")
+elif alien_color == "yellow":
+    print("Nailed it for 10 points!")
+else:
+    print("Bullseye! That's 15 points.")
+
+#exercise showing result of shooting a yellow alien
+alien_color = "yellow"
+
+if alien_color == "green":
+    print("Nice shot! You earned 5 points.")
+elif alien_color == "yellow":
+    print("Nailed it for 10 points!")
+else:
+    print("Bullseye! That's 15 points.")
+
+#exercise showing result of shooting a red alien
+alien_color = "red"
+
+if alien_color == "green":
+    print("Nice shot! You earned 5 points.")
+elif alien_color == "yellow":
+    print("Nailed it for 10 points!")
+else:
+    print("Bullseye! That's 15 points.")
