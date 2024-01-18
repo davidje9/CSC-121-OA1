741d9d4143e8c306f7f5ea42d352f0d50009097d
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-18 08:25:03 -0500

    Add files via upload

---
+++ 5-4_alien_colors_2.py
@@ -0,0 +1,15 @@
+#exercise showing the if condition as true
+alien_color = "green"
+
+if alien_color == "green":
+    print("Nice shot! You earned 5 points.")
+else:
+    print("Bullseye! That's 10 points.")
+
+#exercise showing the else condition as true
+alien_color = "yellow"
+
+if alien_color == "green":
+    print("Nice shot! You earned 5 points.")
+else:
+    print("Bullseye! That's 10 points.")
