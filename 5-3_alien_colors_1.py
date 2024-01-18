b49c00bd7ea63d97e65669a67947f0574b828918
davidje9 <155984488+davidje9@users.noreply.github.com>
2024-01-18 08:14:33 -0500

    Add files via upload

---
+++ 5-3_alien_colors_1.py
@@ -0,0 +1,13 @@
+#exercise showing a false response that yields no output
+"""
+alien_color = "yellow"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+"""
+
+#exercise showing a true response
+alien_color = "green"
+
+if alien_color == "green":
+    print("You earned 5 points!")
